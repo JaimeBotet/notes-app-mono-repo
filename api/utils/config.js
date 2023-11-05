522c2d740f386360c2a02d8839e2888e74fcf946
jabotet <jaimebotet@klosions.com>
2023-11-05 14:02:47 +0100

    change heroku config

--- api/utils/config.js
+++ api/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 5000
 const jwtSecret = process.env.JWT_SECRET
 const connectionString = process.env.NODE_ENV.trim() === 'test' // trim() to delete white spaces
 ? process.env.MONGO_DB_URI_TEST 
