ba62ee88fc335dfe934dc1184787b43e3503c7bf
jabotet <jaimebotet@klosions.com>
2024-01-21 15:24:08 +0100

    video 38: change Login component logic to LogicForm with useField

--- app/src/Login.js
+++ app/src/Login.js
@@ -1,42 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import LoginForm from './components/LoginFrom';
-import { useNavigate, redirect } from 'react-router-dom';
-import { useUser } from './hooks/useUser'
+import LoginForm from './components/LoginForm';
 
 export default function Login(props){
-	const navigate = useNavigate()
-	const [username, setUsername] = useState('')
-	const [password, setPassword] = useState('')
-	const { user, login } = useUser()
-	const [errorMessage, setErrorMessage] 	= useState('')
-
-	const handleLogin = async (event) => {
-		event.preventDefault()
-
-		try {
-			login({username, password})
-
-			setUsername('')
-			setPassword('')
-			navigate('/notes')
-		} catch (e) {
-			setErrorMessage('Wrong Credentials')
-			setTimeout( () => {
-				setErrorMessage('')
-			}, 5000)
-		}
-	}
-
-	// if(user) return redirect("/")
-	if(errorMessage) return <p>{errorMessage}</p>
-
 	return (
-		<LoginForm 
-			username={username}
-			password={password}
-			handleUsernameChange={({target}) => setUsername(target.value)}
-			handlePasswordChange={({target}) => setPassword(target.value)}
-			handleSubmit={handleLogin}
-		/>
+		<LoginForm />
 	);
 }
---
+++ app/src/components/LoginForm.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react'
+// import PropTypes from 'prop-types'
+import { useUser } from '../hooks/useUser'
+import { useNavigate } from 'react-router-dom';
+
+const useField = ({type}) => {
+	const [value, setValue] = useState('')
+	const onChange = (event) => {
+		setValue(event.target.value)
+	}
+
+	return {
+		type,
+		value,
+		setValue,
+		onChange
+	}
+}
+
+
+export default function LoginForm () {
+	const navigate = useNavigate()
+	const username = useField({type: 'text'})
+	const password = useField({type: 'password'})
+	const { user, login } = useUser()
+	const [errorMessage, setErrorMessage] 	= useState('')
+
+	const handleLogin = async (event) => {
+		event.preventDefault()
+
+		try {
+			login({username: username.value, password: password.value})
+
+			username.setValue('')
+			password.setValue('')
+			navigate('/notes')
+		} catch (e) {
+			setErrorMessage('Wrong Credentials')
+			setTimeout( () => {
+				setErrorMessage('')
+			}, 5000)
+		}
+	}
+
+	if(errorMessage) return <p>{errorMessage}</p>
+
+	return (
+		<form onSubmit={handleLogin}>
+			<div>
+				<input 
+					{...username}
+					name='Username'
+					placeholder='Username'
+				/>
+			</div>
+			<div>
+				<input 
+					{...password}
+					name='Password'
+					placeholder='Password'
+				/>
+			</div>
+			<div>
+				<button>Login</button>
+			</div>
+		</form>
+	)
+}
+
+// LoginForm.propTypes = {
+// 	handleSubmit: PropTypes.func.isRequired
+// }
